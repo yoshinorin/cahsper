011fbd22afa7b4b6a5fa46e03e2c7a558e681167
YoshinoriN <nayo-msoft@outlook.jp>
2019-09-07 10:42:32 +0900

    feat(repository/comment): add findById method

--- cahsper-backend/src/main/scala/net/yoshinorin/cahsper/models/db/CommentRepository.scala
+++ cahsper-backend/src/main/scala/net/yoshinorin/cahsper/models/db/CommentRepository.scala
@@ -17,6 +17,16 @@ class CommentRepository extends QuillService {
   }
 
   /**
+   * Find comment by id
+   *
+   * @param id
+   * @return Comment
+   */
+  def findById(id: Int): Option[Comments] = {
+    run(query[Comments].filter(comment => comment.id == lift(id))).headOption
+  }
+
+  /**
    * Get all records in the comments table
    *
    * TODO: implement like SQL limit and should change function name
