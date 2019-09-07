a6e7621688c1984755a879071cc54fc3ae71eb53
YoshinoriN <nayo-msoft@outlook.jp>
2019-09-07 12:08:47 +0900

    test(config): fix test case name

--- cahsper-backend/src/test/scala/net/yoshinorin/cahsper/config/ConfigSpec.scala
+++ cahsper-backend/src/test/scala/net/yoshinorin/cahsper/config/ConfigSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.WordSpec
 class ConfigSpec extends WordSpec {
 
   "Config" should {
-    "gettable database configuration from application.conf" in {
+    "database configuration gettable from application.conf" in {
       assert(Config.dbUrl == "jdbc:mariadb://127.0.0.1/cahsper?useUnicode=true&characterEncoding=utf8mb4")
       assert(Config.dbUser == "root")
       assert(Config.dbPassword == "pass")
