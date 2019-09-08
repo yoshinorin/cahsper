b10d652fc16a590a0c5a8dbf0256f26b7095587d
YoshinoriN <nayo-msoft@outlook.jp>
2019-09-08 13:59:23 +0900

    feat(http): implement http request logger

---
+++ cahsper-backend/src/main/scala/net/yoshinorin/cahsper/http/HttpLogger.scala
@@ -0,0 +1,20 @@
+package net.yoshinorin.cahsper.http
+
+import akka.event.Logging
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.server.{Directive0, RouteResult}
+import akka.http.scaladsl.server.directives.{DebuggingDirectives, LogEntry}
+
+trait HttpLogger {
+
+  // TODO: Change log format
+  private def requestAndResponseLogging(request: HttpRequest): RouteResult => Option[LogEntry] = {
+    case RouteResult.Complete(response) =>
+      Some(LogEntry(request.method.name + " - " + request.uri + " - " + response.status, Logging.InfoLevel))
+    case RouteResult.Rejected(rejections) =>
+      Some(LogEntry(request.method.name + " - " + request.uri + " - " + rejections, Logging.ErrorLevel))
+  }
+
+  def httpLogging: Directive0 = DebuggingDirectives.logRequestResult(requestAndResponseLogging _)
+
+}
--- cahsper-backend/src/main/scala/net/yoshinorin/cahsper/http/HttpServer.scala
+++ cahsper-backend/src/main/scala/net/yoshinorin/cahsper/http/HttpServer.scala
@@ -3,16 +3,16 @@ package net.yoshinorin.cahsper.http
 import akka.actor.ActorSystem
 import akka.http.scaladsl.server.{HttpApp, Route}
 
-class HttpServer(apiStatusRoute: ApiStatusRoute, commentServiceRoute: CommentServiceRoute)(implicit actorSystem: ActorSystem) extends HttpApp {
+class HttpServer(apiStatusRoute: ApiStatusRoute, commentServiceRoute: CommentServiceRoute)(implicit actorSystem: ActorSystem) extends HttpApp with HttpLogger {
 
   override def startServer(host: String, port: Int): Unit = {
     super.startServer(host, port)
     actorSystem.terminate
   }
 
-  override def routes: Route = {
-    apiStatusRoute.route ~
-      commentServiceRoute.route
-  }
-
+  override def routes: Route =
+    httpLogging {
+      apiStatusRoute.route ~
+        commentServiceRoute.route
+    }
 }
