d35c74d9a490223e35eb03ccdfb68fb0cedd6223
YoshinoriN <nayo-msoft@outlook.jp>
2019-09-18 23:02:53 +0900

    feat(service/users): implement UserService

---
+++ cahsper-backend/src/main/scala/net/yoshinorin/cahsper/services/UserService.scala
@@ -0,0 +1,39 @@
+package net.yoshinorin.cahsper.services
+
+import net.yoshinorin.cahsper.models.db.{UserRepository, Users}
+import scala.concurrent.{ExecutionContext, Future}
+
+class UserService(userRepository: UserRepository)(implicit executionContext: ExecutionContext) {
+
+  private[this] def create(user: Users): Future[String] = Future {
+    userRepository.insert(user)
+  }
+
+  /**
+   * Create User
+   *
+   * @param userName user name
+   * @return
+   */
+  def create(userName: String): Future[Users] = {
+    // TODO: Check user already exists or not
+
+    val user = Users(userName)
+    for {
+      maybeUserName <- this.create(user)
+      maybeUser <- this.findByName(maybeUserName)
+    } yield maybeUser.head
+
+  }
+
+  /**
+   * Find user by name
+   *
+   * @param name user name
+   * @return
+   */
+  def findByName(name: String): Future[Option[Users]] = Future {
+    userRepository.findByName(name)
+  }
+
+}
---
+++ cahsper-backend/src/test/scala/net/yoshinorin/cahsper/services/UserServiceSpec.scala
@@ -0,0 +1,47 @@
+package net.yoshinorin.cahsper.services
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import net.yoshinorin.cahsper.models.db.{UserRepository, Users}
+import org.mockito.Mockito._
+import org.scalatest.WordSpec
+import org.scalatestplus.mockito.MockitoSugar
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContextExecutor}
+import scala.util.{Failure, Success}
+
+// testOnly *UserServiceSpec
+class UserServiceSpec extends WordSpec with MockitoSugar {
+
+  implicit val actorSystem: ActorSystem = ActorSystem("cahsper")
+  implicit val actorMaterializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContextExecutor: ExecutionContextExecutor = actorSystem.dispatcher
+
+  val mockUserRepository: UserRepository = mock[UserRepository]
+
+  when(mockUserRepository.findByName("YoshinoriN"))
+    .thenReturn(Some(Users("YoshinoriN", 1567814290)))
+
+  when(mockUserRepository.insert(Users("YoshinoriN")))
+    .thenReturn("YoshinoriN")
+
+  val userService: UserService = new UserService(mockUserRepository)
+
+  "UserService" should {
+
+    "find users instance when call findById with an argument is 1" in {
+      val result = Await.result(userService.findByName("YoshinoriN"), Duration.Inf)
+      assert(result == Some(Users("YoshinoriN", 1567814290)))
+    }
+
+    "create new user" in {
+      userService.create("YoshinoriN").onComplete {
+        case Success(user) => assert(user.name == "YoshinoriN")
+        case Failure(exception) => // Nothing to do
+      }
+    }
+
+  }
+
+}
